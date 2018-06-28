cb52db5633c53fafe69244189a034068b56efa7e
Jose Guillermo Garcia Serrano <joseg1224@hotmail.com>
2018-06-28 13:24:29 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forloop(array){
+for (var i = 0; i <= 25; i++){
+ if(i === 1){
+   array.push("I am ${i} strange loops.")
+   } else {
+     array.push("I am ${i} strange loops.")
+   }
+ }
+}
+return array
+var x = 50;
+function whileloop(x){
+  while (x > 50){
+  console.log(--x)
+  }
+  return "done"
+}
